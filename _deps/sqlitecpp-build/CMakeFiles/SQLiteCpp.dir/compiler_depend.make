ceaa1dcaa9d0f3c34393eab8395f6bab45ee8cd6
jdr0002 <jdr0002@uah.edu>
2025-03-30 19:20:27 -0500

    Fix for errant deps folder outside build directory again.

--- _deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SQLiteCpp.
-# This may be replaced when dependencies are built.
--- _deps/sqlitecpp-build/sqlite3/CMakeFiles/sqlite3_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sqlite3_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sqlite3_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
