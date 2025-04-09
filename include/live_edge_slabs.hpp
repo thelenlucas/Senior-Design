d1dc950eeaf3a103df07a8c6c9e61087294dd84f
Lucas Thelen <thelenlucas028@gmail.com>
2025-04-09 17:57:56 -0500

    Oops
    Needed to smush this down so that it would work

--- include/live_edge_slabs.hpp
+++ include/live_edge_slabs.hpp
@@ -34,112 +34,6 @@ struct InProgressSlab
 };
 
 class Database;
-class SlabManufacturer
-{
-private:
-    Log *original_log;
-    int log_len_eighths;
-    int log_diameter_eighths;
-    int current_used_diameter_eighths;
-    std::vector<InProgressSlab> madeCuts;
-    std::vector<InProgressSlab> undoQueue;
-
-    int widthAtOffsetFromCurrentEighths(int offset)
-    {
-        int midpoint = this->log_diameter_eighths / 2;
-        int from_midpoint = abs(midpoint - this->current_used_diameter_eighths + offset);
-
-        return 2 * sqrt(pow((this->log_diameter_eighths / 2), 2) - pow(from_midpoint, 2));
-    }
-
-public:
-    SlabManufacturer(Log &log)
-    {
-        this->log_len_eighths = log.getLenQuarters() * 2;
-        this->log_diameter_eighths = log.getDiameterQuarters() * 2;
-        this->current_used_diameter_eighths = 8;
-        madeCuts = std::vector<InProgressSlab>();
-        undoQueue = std::vector<InProgressSlab>();
-        this->original_log = &log;
-    }
-
-    int nextCutWouldBeEightsThick(int thickness_eighths)
-    {
-        int distance_from_current = this->widthAtOffsetFromCurrentEighths(0);
-        int distance_from_cut = this->widthAtOffsetFromCurrentEighths(thickness_eighths);
-
-        return (distance_from_cut + distance_from_current) / 2;
-    }
-
-    bool makeSlice(int thickness_eighths, int kerf_eights)
-    {
-        // Check if the cut is possible
-        if (this->current_used_diameter_eighths + thickness_eighths + kerf_eights)
-        {
-            return false;
-        }
-
-        // Get the width of the cut board
-        int board_width = this->nextCutWouldBeEightsThick(thickness_eighths);
-        InProgressSlab slab = {board_width, thickness_eighths, kerf_eights};
-        this->madeCuts.push_back(slab);
-        this->current_used_diameter_eighths += thickness_eighths + kerf_eights;
-
-        return true;
-    }
-
-    // Returns false if there are no cuts to undo, true otherwise
-    bool undo()
-    {
-        // Take the last cut, remove it from the madecuts and add it to the undo queue.
-        if (this->madeCuts.size() == 0)
-        {
-            return false;
-        }
-        InProgressSlab lastCut = this->madeCuts.back();
-        this->madeCuts.pop_back();
-        this->undoQueue.push_back(lastCut);
-        this->current_used_diameter_eighths -= lastCut.thickness_eighths + lastCut.kerf_eighths;
-        return true;
-    }
-
-    // Returns false if there are no cuts to redo, true otherwise
-    bool redo()
-    {
-        // Take the last cut added to the undo queue, remove it from the undo queue and add it to the made cuts.
-        if (this->undoQueue.size() == 0)
-        {
-            return false;
-        }
-
-        InProgressSlab lastCut = this->undoQueue.back();
-        this->undoQueue.pop_back();
-        this->madeCuts.push_back(lastCut);
-        this->current_used_diameter_eighths += lastCut.thickness_eighths + lastCut.kerf_eighths;
-        return true;
-    }
-
-    int availableQuarters()
-    {
-        return this->original_log->getDiameterQuarters() * 2 - this->current_used_diameter_eighths;
-    };
-
-    // Produce all of the cuts using the static functions in Slab
-    // and insert them into the database
-    std::vector<Slab> finalize(std::string location = "", std::string notes = "")
-    {
-        // Simply shove all of our in progress logs into CutAllSlabs
-        std::vector<Slab> slabs = Slab::CutAllSlabs(*this->original_log, this->madeCuts, this->log_len_eighths, location, notes);
-
-        // Clear the made cuts and undo queue
-        this->madeCuts.clear();
-        this->undoQueue.clear();
-
-        // Return the slabs
-        return slabs;
-    }
-
-};
 
 // DDL:
 // CREATE TABLE slabs (
@@ -307,4 +201,111 @@ public:
     bool getSmoothed() { return smoothed; }
     std::string getLocation() { return location; }
     std::string getNotes() { return notes; }
+};
+
+class SlabManufacturer
+{
+private:
+    Log *original_log;
+    int log_len_eighths;
+    int log_diameter_eighths;
+    int current_used_diameter_eighths;
+    std::vector<InProgressSlab> madeCuts;
+    std::vector<InProgressSlab> undoQueue;
+
+    int widthAtOffsetFromCurrentEighths(int offset)
+    {
+        int midpoint = this->log_diameter_eighths / 2;
+        int from_midpoint = abs(midpoint - this->current_used_diameter_eighths + offset);
+
+        return 2 * sqrt(pow((this->log_diameter_eighths / 2), 2) - pow(from_midpoint, 2));
+    }
+
+public:
+    SlabManufacturer(Log &log)
+    {
+        this->log_len_eighths = log.getLenQuarters() * 2;
+        this->log_diameter_eighths = log.getDiameterQuarters() * 2;
+        this->current_used_diameter_eighths = 8;
+        madeCuts = std::vector<InProgressSlab>();
+        undoQueue = std::vector<InProgressSlab>();
+        this->original_log = &log;
+    }
+
+    int nextCutWouldBeEightsThick(int thickness_eighths)
+    {
+        int distance_from_current = this->widthAtOffsetFromCurrentEighths(0);
+        int distance_from_cut = this->widthAtOffsetFromCurrentEighths(thickness_eighths);
+
+        return (distance_from_cut + distance_from_current) / 2;
+    }
+
+    bool makeSlice(int thickness_eighths, int kerf_eights)
+    {
+        // Check if the cut is possible
+        if (this->current_used_diameter_eighths + thickness_eighths + kerf_eights)
+        {
+            return false;
+        }
+
+        // Get the width of the cut board
+        int board_width = this->nextCutWouldBeEightsThick(thickness_eighths);
+        InProgressSlab slab = {board_width, thickness_eighths, kerf_eights};
+        this->madeCuts.push_back(slab);
+        this->current_used_diameter_eighths += thickness_eighths + kerf_eights;
+
+        return true;
+    }
+
+    // Returns false if there are no cuts to undo, true otherwise
+    bool undo()
+    {
+        // Take the last cut, remove it from the madecuts and add it to the undo queue.
+        if (this->madeCuts.size() == 0)
+        {
+            return false;
+        }
+        InProgressSlab lastCut = this->madeCuts.back();
+        this->madeCuts.pop_back();
+        this->undoQueue.push_back(lastCut);
+        this->current_used_diameter_eighths -= lastCut.thickness_eighths + lastCut.kerf_eighths;
+        return true;
+    }
+
+    // Returns false if there are no cuts to redo, true otherwise
+    bool redo()
+    {
+        // Take the last cut added to the undo queue, remove it from the undo queue and add it to the made cuts.
+        if (this->undoQueue.size() == 0)
+        {
+            return false;
+        }
+
+        InProgressSlab lastCut = this->undoQueue.back();
+        this->undoQueue.pop_back();
+        this->madeCuts.push_back(lastCut);
+        this->current_used_diameter_eighths += lastCut.thickness_eighths + lastCut.kerf_eighths;
+        return true;
+    }
+
+    int availableQuarters()
+    {
+        return this->original_log->getDiameterQuarters() * 2 - this->current_used_diameter_eighths;
+    };
+
+    // Produce all of the cuts using the static functions in Slab
+    // and insert them into the database
+    std::vector<Slab> finalize(std::string location = "", std::string notes = "")
+    {
+        // Simply shove all of our in progress logs into CutAllSlabs
+        std::vector<Slab> slabs = Slab::CutAllSlabs(*this->original_log, this->madeCuts, this->log_len_eighths, location, notes);
+
+        // Clear the made cuts and undo queue
+        this->madeCuts.clear();
+        this->undoQueue.clear();
+
+        // Return the slabs
+        return slabs;
+    }
+
 };
