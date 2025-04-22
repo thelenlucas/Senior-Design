b3e21f04c3dae1d6c75a623f845b2ddb468f71e6
Zack Deal <119079277+Zack812218@users.noreply.github.com>
2025-04-21 22:34:23 -0500

    Log importing functionality
    It finally works again

--- src/csv_importer.cpp
+++ src/csv_importer.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 #include "csv_importer.hpp"
+#include "domain/log.hpp"
 
 std::vector<std::string> Importer::digestLine(const std::string& line){
     std::vector<std::string> parts;
@@ -14,24 +15,24 @@ std::vector<std::string> Importer::digestLine(const std::string& line){
     return parts;
 }
 
-// woodworks::domain::types::Drying Importer::returnDryingType(std::string dryStr)
-// {
-//     std::transform(dryStr.begin(), dryStr.end(), dryStr.begin(), ::toupper);
-//     if(dryStr == "KILN DRIED" || dryStr == "KILN") return 1;
-//     else if(dryStr == "AIR DRIED" || dryStr == "AIR") return 2;
-//     else if(dryStr == "AIR AND KILN DRIED" || dryStr == "KILN AND AIR DRIED" || dryStr == "AIR AND KILN" || dryStr == "KILN AND AIR") return 3;
-//     else return 0;
-// }
+woodworks::domain::types::Drying Importer::returnDryingType(std::string dryStr)
+{
+    std::transform(dryStr.begin(), dryStr.end(), dryStr.begin(), ::toupper);
+    if(dryStr == "KILN DRIED" || dryStr == "KILN") return Drying::KILN_DRIED;
+    else if(dryStr == "AIR DRIED" || dryStr == "AIR") return Drying::AIR_DRIED;
+    else if(dryStr == "AIR AND KILN DRIED" || dryStr == "KILN AND AIR DRIED" || dryStr == "AIR AND KILN" || dryStr == "KILN AND AIR") return Drying::KILN_AND_AIR_DRIED;
+    else return Drying::GREEN;
+}
 
 //	   SLAB_SURFACNG: 
 //	   woodworks::domain::types::SlabSurfacing
 //	   RGH (rough), S1S, S2S
 
-//	   LUMUBER_SURFACNG:
+//	   LUMBER_SURFACNG:
 //     woodworks::domain::types::LumberSurfacing
 //     RGH, S1S, S2S, S3S, S4S
 
-// bool Importer::returnSmoothed(std::string smd)
+// uint Importer::returnSmoothed(std::string smd)
 // {
 //     std::transform(smd.begin(), smd.end(), smd.begin(), ::toupper);
 //     if(smd == "Y" || smd == "YES") return true;
@@ -39,35 +40,52 @@ std::vector<std::string> Importer::digestLine(const std::string& line){
 // }
 
 
-// void Importer::importLogs(const std::string& filePath){
-//     //length (ft/in), diameter (in), species, quality, drying, cost, location, notes
-//     //length, length, species, quality, drying, dollar, string, string
-//     std::ifstream file(filePath);
-//     if(!file){
-//         std::cout << "File could not open at: " << filePath << std::endl;
-//         return;
-//     }
+void Importer::importLogs(const std::string& filePath){
+    //length (ft/in), diameter (in), species, quality, drying, cost, location, notes
+    //length, length, species, quality, drying, dollar, string, string
+    std::ifstream file(filePath);
+    if(!file){
+        std::cout << "File could not open at: " << filePath << std::endl;
+        return;
+    }
 
-//     std::string line;
-//     if(!std::getline(file, line)){return;}
+    std::string line;
+    if(!std::getline(file, line)){return;}
 
-//     while(std::getline(file, line)){
-//         if(line.empty()){continue;}
-//         auto cols = digestLine(line);
-        
-//         std::string species     = cols[1];
-//         uint lenQ           = std::stoul(cols[2]);
-//         uint diamQ          = std::stoul(cols[3]);
-//         double cost             = std::stod(cols[4]);
-//         int quality             = std::stoi(cols[5]);
-//         Drying drying           = returnDryingType(cols[6]);
-//         std::string location    = (cols.size() > 7 && !cols[7].empty()) ? cols[7] : "";
-//         std::string notes       = (cols.size() > 8 && !cols[8].empty()) ? cols[8] : "";
+    woodworks::domain::Log log = woodworks::domain::Log::uninitialized();
 
-//         Log newLog(-1, species, lenQ, diamQ, cost, quality, drying, location, notes);
-//         newLog.insert();
-//     }
-// }
+    while(std::getline(file, line)){
+        if(line.empty()){continue;}
+        auto cols = digestLine(line);
+
+        cols[2].erase(std::remove(cols[2].begin(), cols[2].end(),'\"'), cols[2].end());
+        size_t pos = cols[2].find('\'');
+        std::string ft = cols[2].substr(0, pos);
+        std::string in = cols[2].substr(pos + 1);
+
+        Species logSpecies          = {cols[1]};
+        Length logLen               = Length::fromFeet(std::stod(ft)) + Length::fromInches(std::stod(in));
+        Length logDiam              = Length::fromInches(std::stoul(cols[3]));
+        Dollar logCost              = {static_cast<int>(std::stod(cols[4])*100)};
+        Quality logQuality          = {std::stoi(cols[5])};
+        Drying logDrying            = returnDryingType(cols[6]);
+        std::string location        = (cols.size() > 7 && !cols[7].empty()) ? cols[7] : "";
+        std::string notes           = (cols.size() > 8 && !cols[8].empty()) ? cols[8] : "";
+
+        log.length                  = logLen;
+        log.diameter                = logDiam;
+        log.species                 = logSpecies;
+        log.quality                 = logQuality;
+        log.drying                  = logDrying;
+        log.cost                    = logCost;
+        log.location                = location;
+        log.notes                   = notes;
+
+        auto &db = woodworks::infra::DbConnection::instance();
+        auto repo = woodworks::infra::QtSqlRepository<woodworks::domain::Log>(db);
+        if(!repo.add(log)) {std::cerr << "Failed to insert log: " + db.lastError().text().toStdString() << std::endl;}
+     }
+}
 
 // void Importer::importFirewood(const std::string& filePath){
 //     //id, species, drying, feet^3, location, notes
